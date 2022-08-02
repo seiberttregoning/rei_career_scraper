0310a731a0968c0bec799daad5c931685bb1d708
seiberttregoning <georgetregoning@gmail.com>
2022-08-02 10:17:09 -0500

    initial commit, working code

---
+++ REI_job_scraper.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import requests
+
+from bs4 import BeautifulSoup
+
+import smtplib
+
+from email.message import EmailMessage
+
+from datetime import datetime, timedelta, date
+
+
+yesterday = date.today() - timedelta(days=1)
+
+brentwood_rei_url = 'https://rei.jobs/careers/SearchJobs/?3_73_3=37158&jobSort=postedDate&jobSortDirection=DESC&'
+lebanon_rei_url = 'https://rei.jobs/careers/SearchJobs/?3_73_3=5813561&jobSort=postedDate&jobSortDirection=DESC&'
+chattanooga_rei_url = 'https://rei.jobs/careers/SearchJobs/?3_73_3=2217122&jobSort=postedDate&jobSortDirection=DESC&'
+
+
+def get_job_titles(url, dict_to_append):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    joblist = soup.find('ul', class_='jobList')
+    for li in joblist.find_all('li', class_="group"):
+        title = li.find('a').get('title')
+        link = li.find('a').get('href')
+        if li.find('p') is None:
+            pass
+        else:
+            date_str = li.find('p').find_all('span')[1].text.split(' ', 1)[1]
+            date_obj = datetime.strptime(date_str, '%m-%d-%Y').date()
+
+        dict_to_append.update({link: [title, date_obj]})
+
+
+job_dict = {}
+
+get_job_titles(brentwood_rei_url, job_dict)
+get_job_titles(lebanon_rei_url, job_dict)
+get_job_titles(chattanooga_rei_url, job_dict)
+
+
+new_jobs_str = ''
+
+for (k, v) in job_dict.items():
+    if v[1] > yesterday:
+        new_jobs_str += f'{str(v[0])}\n{k}\n\n'
+    else:
+        pass
+
+
+if len(new_jobs_str) == 0:
+    print('No new jobs')
+else:
+    smtpHost = 'smtp.office365.com'
+    smtpPort = 587
+    sender = 'georgetregoning@gmail.com'
+    with open(r'C:\Users\georg\Documents\REI Download\outlookpassword.txt') as f:
+        password = f.readlines()[0]
+
+    receiver = ['annie.graefe@gmail.com']
+
+    subject = "New Brentwood-Chatt-Lebanon REI Jobs"
+    # Add the From: and To: headers at the start!
+    message = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
+               % (sender, ", ".join(receiver), subject))
+    message += new_jobs_str
+
+    try:
+        smtpObj = smtplib.SMTP(smtpHost, smtpPort)
+        # smtpObj.set_debuglevel(1)
+        smtpObj.ehlo()
+        smtpObj.starttls()
+        smtpObj.ehlo()
+        smtpObj.login(sender, password)
+        smtpObj.sendmail(sender, receiver, message)
+        smtpObj.quit()
+        print("Successfully sent email")
+    except:
+        print("Error: unable to send email")
